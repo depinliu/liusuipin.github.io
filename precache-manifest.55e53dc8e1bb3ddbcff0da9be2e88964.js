e0fea64857f81ef2892e2dd16df32310ca11dfc9
liusuipin <liuprogramming@gmail.com>
2019-10-19 21:02:27 +0700

    add build file

---
+++ precache-manifest.55e53dc8e1bb3ddbcff0da9be2e88964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76a91d1278c0c750a4e9b2b070fbe889",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0e51147f0f680214207a",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bd1b67f132e34f24935e",
+    "url": "/static/css/main.d1189a7b.chunk.css"
+  },
+  {
+    "revision": "0e51147f0f680214207a",
+    "url": "/static/js/2.4b626c3c.chunk.js"
+  },
+  {
+    "revision": "bd1b67f132e34f24935e",
+    "url": "/static/js/main.4e3a6dbd.chunk.js"
+  },
+  {
+    "revision": "e49c1f105de0f86fc2e4",
+    "url": "/static/js/runtime-main.0fd3eeee.js"
+  },
+  {
+    "revision": "2ac88783c8b23b0a054b69f073ae3679",
+    "url": "/static/media/devin.2ac88783.png"
+  },
+  {
+    "revision": "c3f68ddb5120759b4f95a911db62cf6c",
+    "url": "/static/media/resume.c3f68ddb.pdf"
+  }
+]);
